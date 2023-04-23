3d0fc7563a125f87eaaf66ea786b1a0bfe6f3d28
Alex <alex.soderberg97@gmail.com>
2023-04-23 22:23:31 +0200

    MVP

---
+++ src/components/Lamp.vue
@@ -0,0 +1,78 @@
+<script setup>
+defineProps({
+  lightsOn: {
+    type: Boolean,
+    required: true
+  }
+})
+// export default {
+//   name: 'troll',
+// }
+</script>
+
+<template>
+  <div class="text" id="cord">
+  </div>
+
+  <div id="lamp">
+
+    <div class="text" id="socket">
+    </div>
+    
+    <br>
+    
+    <div class="text"  id="bulb">
+      <span id="coil">
+        w
+      </span>
+    </div>
+
+  </div>
+
+  <div id="light">
+
+  </div>
+      
+</template>
+<style>
+  #coil {
+    font-size:0.75rem;
+  }
+  #light {
+    z-index:3;
+    height:350px;
+    aspect-ratio: 1;
+    position:absolute;
+    margin:0px!important;
+    top: 50%;
+    left:50%;
+    background:none;
+    border-radius:50%;
+    filter: blur(800px);
+  }
+  #lamp {
+    display:flex; 
+    justify-content: center; 
+    align-items:center; 
+    flex-direction:column; 
+    margin-bottom:1rem;
+  }
+  #cord {
+    height:10vh; 
+    width:50%; 
+    border-right:2px solid black; 
+    display:flex; 
+  }
+  #socket {
+    height:2vh; 
+    width:1.5vh; 
+    border:2px solid black; 
+  }
+  #bulb {
+    height:5vh;
+    width:5vh; 
+    border-radius: 50%; 
+    border:2px solid black;
+    margin-top:-2px;
+  }
+</style>
